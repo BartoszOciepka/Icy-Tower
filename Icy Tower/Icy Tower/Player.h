df5d40c49fa1b28e30717a81813c671f1572f97a
Bartosz Ociepka <Bartosz Ociepka@DESKTOP-GDC9AD4>
2016-11-29 21:15:54 +0100

    Added character bitmap

---
+++ Icy Tower/Icy Tower/Player.h
@@ -0,0 +1,9 @@
+#pragma once
+class Player
+{
+public:
+	Player();
+	~Player();
+	//ALLEGRO_BITMAP * player_bitmap;
+};
+
--- Icy Tower/Icy Tower/main.cpp
+++ Icy Tower/Icy Tower/main.cpp
@@ -1,28 +1,34 @@
-#include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <windows.h>
+#include "Player.h"
 
-int main(int argc, char **argv)
+int main()
 {
-	ALLEGRO_DISPLAY *display = NULL;
-
-	if (!al_init()) {
-		fprintf(stderr, "failed to initialize allegro!\n");
-		return -1;
+	Player player;
+	al_init(); // inicjowanie biblioteki allegro
+	al_install_keyboard(); // instalowanie sterownika klawiatury
+	al_init_image_addon();// inicjowanie dodatku umo¿liwiaj¹cego odczyt jak i zapis obrazów, w formatach BMP,  PNG, JPG, PCX, TGA.
+	ALLEGRO_KEYBOARD_STATE klawiatura; // utworzenie struktury do odczytu stanu klawiatury
+	ALLEGRO_DISPLAY *okno = al_create_display(640, 600);// tworzymy wskaŸnik okna, i podajemy jego szer. i wys
+	al_set_window_title(okno, "Icy Tower");// podajemy tytu³ okna
+	ALLEGRO_BITMAP *obrazek = al_load_bitmap("icytower-bck.png");// wczytujemy bitmapê do pamiêci
+	//player-> 
+	ALLEGRO_BITMAP * player_bitmap = al_load_bitmap("champ.png");
+	float i = 30;
+	while (!al_key_down(&klawiatura, ALLEGRO_KEY_A) && i < 700) //koniec programu gdy wciœniemy klawisz Escape
+	{
+		al_get_keyboard_state(&klawiatura);  // odczyt stanu klawiatury
+		al_clear_to_color(al_map_rgb(0, 255, 0)); // wyczyszczenie aktualnego bufora ekranu
+		al_draw_bitmap(obrazek, 0, 0, 0);  // wyœwietlenie bitmapy "obrazek" na "Backbuffer" (bufor ekranu)
+		al_draw_bitmap(player_bitmap, i, 530, 0);
+		al_flip_display(); // wyœwietlenie aktualnego bufora na ekran
+		i += 1;
+		Sleep(20);
 	}
-
-	display = al_create_display(640, 480);
-	if (!display) {
-		fprintf(stderr, "failed to create display!\n");
-		return -1;
-	}
-
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-
-	al_flip_display();
-
-	al_rest(10.0);
-
-	al_destroy_display(display);
+	// usuwanie z pamiêci okna, bitmap, audio, fontów ...itd.
+	al_destroy_display(okno);
+	al_destroy_bitmap(obrazek);
 
 	return 0;
 }
